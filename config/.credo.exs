7a02d565e48f9fd046124072d666320603d3b449
Javier Solana Huertas <javier.solana@cabify.com>
2023-11-01 10:37:02 +0100

    included / excluded

--- config/.credo.exs
+++ config/.credo.exs
@@ -5,7 +5,9 @@
       requires: ["./lib/cabify_credo/checks/**/*.ex"],
       checks: [
         {MyCredo.Checks.MyFirstCredoCheck, []}
-      ]
+      ],
+      included: ["lib/", "src/", "web/", "apps/"],
+      excluded: []
     }
   ]
 }
